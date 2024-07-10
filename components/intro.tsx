97cd668b72b8958110df9422dde4d23eac84cfb1
MadR7 <mragni108@gmail.com>
2024-07-10 01:09:30 -0400

    Typewriter effect added

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,14 +1,16 @@
 "use client";
 
 import Image from 'next/image';
-import React from 'react';
+import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import Link from 'next/link';
 import { BsArrowRight, BsLinkedin } from 'react-icons/bs';
 import { HiDownload } from 'react-icons/hi';
 import { FaGithubSquare } from 'react-icons/fa';
+import Typewriter from 'typewriter-effect';
 
 export default function Intro() {
+  const [showButtons, setShowButtons] = useState(false);
   return (
     <section className='relative z-10 mb-28 max-w-[50rem] text-center sm:mb-0'>
       <div className="flex items-center justify-center">
@@ -42,59 +44,69 @@ export default function Intro() {
         >
         </motion.span>
       </div>
-      <motion.p className="text-black mt-10 mb-10 px-4 text-xl font-medium leading-[1.5]"
+      
+      <motion.div className="text-black mt-10 mb-10 px-4 text-xl font-medium leading-[1.5]"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        Hi! I'm
-        <span className='font-bold'> Madhav Rapelli. </span> I'm a student at{" "}
-        <span className="font-bold">Stony Brook University</span> studying
-        <span className="font-bold"> Computer Science</span> and{" "}
-        <span className="font-bold"> Applied Math and Statistics</span>.
-        I'm passionate about
-        <span className="font-bold"> Robotics</span> and{" "}
-        <span className="font-bold"> Machine Learning</span>. But currently,
-        I'm learning Web Development using
-        <span className="font-bold"> Next.js</span>,
-        <span className="font-bold"> TypeScript</span> and
-        <span className="font-bold"> Tailwind CSS</span>.
-      </motion.p>
-      <motion.div className="flex flex-col sm:flex-row justify-center items-center gap-2 px-4 text-lg font-medium"
-        initial={{ opacity: 0, x: 100 }}
-        animate={{ opacity: 1, x: 0 }}
-        transition={{
-          delay: 0.2
+        <Typewriter
+        onInit={(typewriter) => {
+          typewriter
+            .changeDelay(20)
+            .typeString("Hi! I'm <span class='font-bold'>Madhav Rapelli.</span>")
+            .pauseFor(1000)
+            .changeDelay(10)
+            .typeString(" I'm a student at <span class='font-bold'>Stony Brook University</span>")
+            .changeDelay(10)
+            .typeString(" studying <span class='font-bold'>Computer Science</span> and <span class='font-bold'>Applied Math and Statistics</span>.")
+            .changeDelay(5)
+            .typeString(" I'm passionate about <span class='font-bold'>Robotics</span> and <span class='font-bold'>Machine Learning</span>.")
+            .pauseFor(300)
+            .callFunction(() => {
+              setShowButtons(true);
+            })
+            .start();
         }}
-      >
-        <Link href="#contact" className="group bg-black text-white px-7 py-3 flex items-center
-            gap-2 rounded-full outline-none hover:scale-105 hover:bg-gray-950 active:scale-105
-            ">
-          <span>Contact me!</span>
-          <BsArrowRight
-            className="opacity-70 group-hover:translate-x-2 transition"
-          />
-        </Link>
-
-        <a className="group bg-gray-200
-            border-black  px-7 py-3 flex items-center
-            gap-2 rounded-full outline-none hover:scale-105 active:scale-105
-            ">
-          Resume
-          <HiDownload
-            className="group-hover:translate-y-0.5 transition"
-          />
-        </a>
-
-        <a className="group bg-gray-200 p-4 text-gray-700 flex items-center gap-2 rounded-full outline-none hover:scale-125 active:scale-125">
-          <BsLinkedin
-            className="group-hover-translate-y-1 transition"
-          />
-        </a>
-
-        <a className="group bg-gray-200 p-4 text-gray-700 flex items-center gap-2 rounded-full outline-none hover:scale-125 active:scale-125">
-          <FaGithubSquare />
-        </a>
+      />
       </motion.div>
+
+      {showButtons && (
+  <motion.div 
+    className="flex flex-col sm:flex-row justify-center items-center gap-2 px-4 text-lg font-medium"
+    initial={{ opacity: 0, y: 20 }}
+    animate={{ opacity: 1, y: 0 }}
+    transition={{ duration: 0.5 }}
+  >
+    <Link href="#contact" className="group bg-black text-white px-7 py-3 flex items-center
+        gap-2 rounded-full outline-none hover:scale-105 hover:bg-gray-950 active:scale-105
+        ">
+      <span>Contact me!</span>
+      <BsArrowRight
+        className="opacity-70 group-hover:translate-x-2 transition"
+      />
+    </Link>
+
+    <a className="group bg-gray-200
+        border-black  px-7 py-3 flex items-center
+        gap-2 rounded-full outline-none hover:scale-105 active:scale-105
+        ">
+      Resume
+      <HiDownload
+        className="group-hover:translate-y-0.5 transition"
+      />
+    </a>
+
+    <a className="group bg-gray-200 p-4 text-gray-700 flex items-center gap-2 rounded-full outline-none hover:scale-125 active:scale-125">
+      <BsLinkedin
+        className="group-hover-translate-y-1 transition"
+      />
+    </a>
+
+    <a className="group bg-gray-200 p-4 text-gray-700 flex items-center gap-2 rounded-full outline-none hover:scale-125 active:scale-125">
+      <FaGithubSquare />
+    </a>
+  </motion.div>
+)}
     </section>
   );
 }
