4fc25eef31c11e9f5075e3be6e10c8e2187e34de
MadR7 <mragni108@gmail.com>
2024-08-16 21:04:50 -0400

    added docsage to projects

--- lib/data.ts
+++ lib/data.ts
@@ -2,6 +2,7 @@ import personalwebsite from "@/public/personalwebsite.png"
 import vexrobotics from "@/public/vexrobotics.png"
 import scioly from "@/public/scienceolympiadrobot.png"
 import askthebot from "@/public/askthebot.png"
+import docsage from "@/public/docsage.png"
 export const links = [
   {
     name: "Home",
@@ -34,6 +35,12 @@ export const links = [
 
 export const projectsData = [
   {
+    title: "DocSage (in progress)", 
+    description: "This web app can take in a PDF under 10mb and then store it an AWS S3 Bucket. Then later the PDF is broken into chunks and stored in a vector database(PineconeDB). The user can later ask a chatbot powered by OpenAI any question regarding the PDF and it will be able to answer it",
+    tags: ["NextJS", "React", "TailwindCSS", "TypeScript", "PineconeDB", "AWS S3", "RAG"],
+    imageUrl: docsage
+  },
+  {
     title: "Ask The Bot",
     description:
       "This web app can take in any article's URL and then understand it so the user can ask any questions they have about the article. This was made possible because of Upstash's free services. This uses Meta's LLAMA 3 8b LLM to understand the article and answer questions.",
@@ -41,14 +48,7 @@ export const projectsData = [
     imageUrl: askthebot
     
   },
-  {
-    title: "Personal Website",
-    description:
-      "This website displays information about me including my projects, skills and ways to contact me. This is a personal website that I created as a way to learn HTML, CSS, React, TypeScript, NextJS and Framer Motion. ",
-    tags: ["NextJS", "React", "TailwindCSS", "FramerMotion", "TypeScript"],
-    imageUrl: personalwebsite
-    
-  },
+  
   {
     title: "VEX Robotics Program",
     description:
@@ -62,6 +62,14 @@ export const projectsData = [
       "I worked in a team of 2 to build a robot completely from scratch by using CAD, 3d Printed Chassis, Arduino to program it to move in a specific arrangement at the Science Olympiad competition. To achieve this we used Servo Motors as wheels and we wrote a PID program so that the robot would move precisely in a certain path. ",
     tags: ["Arduino", "C++", "PID", "Autonomous Movement"],
     imageUrl: scioly
+  },
+  {
+    title: "Personal Website",
+    description:
+      "This website displays information about me including my projects, skills and ways to contact me. This is a personal website that I created as a way to learn HTML, CSS, React, TypeScript, NextJS and Framer Motion. ",
+    tags: ["NextJS", "React", "TailwindCSS", "FramerMotion", "TypeScript"],
+    imageUrl: personalwebsite
+    
   },
 ] as const;
 
