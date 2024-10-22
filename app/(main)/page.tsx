3b48c38df9933ff9e6628b34be1cc74b4f470680
MadR7 <mragni108@gmail.com>
2024-10-22 18:31:43 -0400

    fixed a small error

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -8,9 +8,7 @@ import Waves2 from '@/components/waves2';
 import Waves3 from '@/components/waves3';
 import Skills from '@/components/skills';
 import Waves4 from '@/components/waves4';
-import Contact from '@/components/contact';
 import Contact2 from '@/components/contact2';
-import Header from '@/components/header';
 import SkillsCloud from '@/components/skillsCloud';
 
 export default function Home() {
