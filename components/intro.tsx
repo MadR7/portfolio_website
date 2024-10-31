0a4af30c1671967b068ac6c194ce3c758f072e48
MadR7 <mragni108@gmail.com>
2024-10-31 11:01:54 -0400

    misc

--- components/intro.tsx
+++ components/intro.tsx
@@ -118,7 +118,7 @@ export default function Intro() {
         className="opacity-70 group-hover:translate-x-2 transition"
       />
     </button>
-    <ResumeButton />
+    { /*<ResumeButton /> */}
 
     <a className="group bg-gray-200 p-4 border-black border-b-2 text-gray-700 flex items-center gap-2 rounded-full outline-none cursor-pointer hover:scale-125 hover:text-gray-950 active:scale-125"
     href="https://www.linkedin.com/in/madhav-rapelli-7a986a295/" target="_blank"
